405ba22392f3fef6245dc1922b025e467f274a2b
PesnyaRadugi <mbarazgov02@gmail.com>
2023-05-15 16:09:59 +0300

    Initial commit
    Created Maven project

---
+++ src/main/java/io/project/MIIChatbot/MiiChatbotApplication.java
@@ -0,0 +1,13 @@
+package io.project.MIIChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiiChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiiChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/MIIChatbot/MiiChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.MIIChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiiChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
